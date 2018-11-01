8c3aee1a074167a968a22fe594e423231bfc3780
Szendrényi Ákos <Szendrnyi kos@DESKTOP-SG6VCUU>
2018-11-01 16:44:57 +0100

    Kész

--- src/feladat_6/Feladat_6.java
+++ src/feladat_6/Feladat_6.java
@@ -1,21 +1,34 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package feladat_6;
 
-/**
- *
- * @author Szendrényi Ákos
- */
+import java.util.Scanner;
+
+
 public class Feladat_6 {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        int pont;
+        Scanner input = new Scanner(System.in);
+        
+        
+        System.out.println("Kérem adja meg az elért pontszámót: ");
+       
+        pont= input.nextInt();
+        if(pont<=42){
+            System.out.println("Elégtelen!");
+        }else if(pont <=43 || pont<=57){
+            System.out.println("Elégséges!");
+        }else if(pont<=58 || pont<=72){
+            System.out.println("Közepes!");
+        }else if(pont<=73 || pont<=87){
+            System.out.println("Jó!");
+        }else if(pont<=88 || pont<=100){
+            System.out.println("Jeles!");
+        }else{
+            System.out.println("Hiba!!!");
+        }
     }
     
 }
